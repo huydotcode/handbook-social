89b5a3af3eff511caa2b7bbd7713334601042653
Ngô Huy <117896799+huydotcode@users.noreply.github.com>
2025-11-28 20:50:58 +0700

    ui: update height error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -6,7 +6,7 @@ const ErrorPage = () => {
     return (
         <>
             <Navbar />
-            <section className="flex h-full items-center p-16 dark:bg-transparent dark:text-dark-primary-1">
+            <section className="flex h-screen items-center p-16 dark:bg-transparent dark:text-dark-primary-1">
                 <div className="container mx-auto my-8 flex flex-col items-center justify-center px-5">
                     <div className="max-w-md text-center">
                         <h2 className="mb-8 text-9xl font-extrabold dark:text-dark-primary-1">
