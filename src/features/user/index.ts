e60f9ebc428daa026258f100d645c17793489bed
Ngô Huy <117896799+huydotcode@users.noreply.github.com>
2025-12-31 19:13:56 +0700

    refactor: simplify user hooks export in index.ts

--- src/features/user/index.ts
+++ src/features/user/index.ts
@@ -1,4 +1,4 @@
 export { userApi } from './apis/user.api';
 export { default as UserService } from './services/user.service';
-export { useUsers, useUser, useUserFriends } from './hooks/useUser';
+export * from './hooks/useUser';
 export * from './types/user.types';
