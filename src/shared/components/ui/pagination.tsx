b12a7d57356f81ae471c4ffed3cfbfd6199f2b63
Ngô Huy <117896799+huydotcode@users.noreply.github.com>
2026-01-18 22:51:17 +0700

    chore (ui): update style pagination

--- src/shared/components/ui/pagination.tsx
+++ src/shared/components/ui/pagination.tsx
@@ -8,7 +8,10 @@ const Pagination = ({ className, ...props }: React.ComponentProps<'nav'>) => (
     <nav
         role="navigation"
         aria-label="pagination"
-        className={cn('mx-auto flex w-full justify-center', className)}
+        className={cn(
+            'mx-auto flex w-full justify-end bg-transparent',
+            className
+        )}
         {...props}
     />
 );
