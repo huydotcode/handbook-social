2d94665cf946a858be85c76bf7ae711b0f0c4be6
Ngô Huy <117896799+huydotcode@users.noreply.github.com>
2026-02-01 12:44:45 +0700

    feat (ui): layout admin add bottom navigation on mobile

--- src/app/(admin)/admin/layout.tsx
+++ src/app/(admin)/admin/layout.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { ProtectedRoute } from '@/features/auth';
-import { Navbar } from '@/shared/components/layout';
+import { BottomNavigation, Navbar } from '@/shared/components/layout';
 import { USER_ROLES } from '@/types/entites';
 import React from 'react';
 import { Sidebar } from '../_components';
@@ -19,6 +19,8 @@ const AdminLayout: React.FC<Props> = ({ children }) => {
             <main className="relative top-[56px] ml-[300px] min-h-[calc(100vh-56px)] px-4 pb-10 lg:ml-0 md:px-2">
                 {children}
             </main>
+
+            <BottomNavigation />
         </ProtectedRoute>
     );
 };
