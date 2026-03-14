6b81612ecd8be911242f4fcfc541b752adb802cf
Ngô Huy <117896799+huydotcode@users.noreply.github.com>
2026-03-14 12:53:48 +0700

    feat: improve sidebar news feed page

--- src/app/(routes)/news-feed/_components/Sidebar.tsx
+++ src/app/(routes)/news-feed/_components/Sidebar.tsx
@@ -1,6 +1,8 @@
 'use client';
 import { cn } from '@/lib/utils';
 import SidebarCollapse from '@/shared/components/layout/sidebar/SidebarCollapse';
+import SidebarList from '@/shared/components/layout/sidebar/SidebarList';
+import SidebarTitle from '@/shared/components/layout/sidebar/SidebarTitle';
 import { Icons } from '@/shared/components/ui';
 import { Button } from '@/shared/components/ui/Button';
 import { usePathname, useSearchParams } from 'next/navigation';
@@ -30,46 +32,37 @@ const Sidebar = () => {
 
     return (
         <SidebarCollapse>
-            <div className="px-x w-full py-1">
-                <h1 className="text-2xl font-bold">News Feed</h1>
+            <SidebarTitle href="/news-feed" title="News Feed" />
 
-                <div className="flex w-full flex-col gap-1">
-                    {sidebarItems.map((item) => {
-                        const Icon = () => {
-                            return item.icon;
-                        };
+            <SidebarList className="mt-2">
+                {sidebarItems.map((item) => {
+                    const Icon = () => {
+                        return item.icon;
+                    };
 
-                        const isActive = filter
-                            ? path === item.link.split('?')[0] &&
-                              filter === item.link.split('=')[1]
-                            : path === item.link;
+                    const isActive = filter
+                        ? path === item.link.split('?')[0] && filter === item.link.split('=')[1]
+                        : path === item.link;
 
-                        return (
-                            <Button
-                                key={item.name}
-                                variant="ghost"
-                                className={cn(
-                                    'w-full justify-start rounded-xl py-6 text-base font-normal',
-                                    {
-                                        'bg-primary-1 text-primary-2 dark:bg-dark-primary-1':
-                                            isActive,
-                                    }
-                                )}
-                                href={item.link}
-                            >
-                                <div className="flex items-center gap-2">
-                                    <span className="text-xl">
-                                        <Icon />
-                                    </span>
-                                    <span className="ml-2 text-sm">
-                                        {item.name}
-                                    </span>
-                                </div>
-                            </Button>
-                        );
-                    })}
-                </div>
-            </div>
+                    return (
+                        <Button
+                            key={item.name}
+                            variant="ghost"
+                            className={cn('w-full justify-start rounded-xl py-6 text-base font-normal', {
+                                'bg-primary-1 text-primary-2 dark:bg-dark-primary-1': isActive,
+                            })}
+                            href={item.link}
+                        >
+                            <div className="flex items-center gap-2">
+                                <span className="text-xl">
+                                    <Icon />
+                                </span>
+                                <span className="ml-2 text-sm">{item.name}</span>
+                            </div>
+                        </Button>
+                    );
+                })}
+            </SidebarList>
         </SidebarCollapse>
     );
 };
