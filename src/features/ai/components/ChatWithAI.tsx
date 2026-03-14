8f73f548b4080da6b8e545ba36f7835ceb287c58
Ngô Huy <117896799+huydotcode@users.noreply.github.com>
2026-03-14 12:49:59 +0700

    chore: hidden chat with ani when in message page

--- src/features/ai/components/ChatWithAI.tsx
+++ src/features/ai/components/ChatWithAI.tsx
@@ -26,7 +26,6 @@ const ChatWithAI = () => {
     const [openChat, setOpenChat] = useState<boolean>(false);
     const [messages, setMessages] = useState<AIChatMessage[]>([]);
 
-    const path = usePathname();
     const bottomRef = useRef<HTMLDivElement>(null);
     const form = useForm<IFormData>();
     const {
@@ -36,8 +35,10 @@ const ChatWithAI = () => {
         resetField,
         formState: { isLoading },
     } = form;
-    const { mutateAsync: sendMessageMutation, isPending } =
-        useSendMessageMutation();
+    const { mutateAsync: sendMessageMutation, isPending } = useSendMessageMutation();
+
+    const path = usePathname();
+    const IS_MESSAGE_PAGE = path.includes('/messages');
 
     async function onSubmit(data: IFormData) {
         try {
@@ -62,9 +63,7 @@ const ChatWithAI = () => {
             resetField('text');
 
             const response = await sendMessageMutation({ message: text });
-            const textAI =
-                response.response?.replace(/\*/g, '') ||
-                'Không có phản hồi từ Handbook AI';
+            const textAI = response.response?.replace(/\*/g, '') || 'Không có phản hồi từ Handbook AI';
 
             setMessages((prev) => [
                 {
@@ -79,9 +78,7 @@ const ChatWithAI = () => {
         } catch (error: any) {
             setMessages((prev) => [
                 {
-                    text:
-                        error?.response?.data?.message ||
-                        'Có lỗi khi gửi tin nhắn, vui lòng thử lại sau',
+                    text: error?.response?.data?.message || 'Có lỗi khi gửi tin nhắn, vui lòng thử lại sau',
                     isAI: true,
                     createAt: new Date(),
                 },
@@ -98,16 +95,9 @@ const ChatWithAI = () => {
 
     return (
         <div className="fixed bottom-3 right-3 z-20 w-fit md:bottom-20">
-            {!openChat && (
-                <Button
-                    onClick={() => setOpenChat((prev) => !prev)}
-                    className="h-10 w-10"
-                >
-                    <Image
-                        src={'/assets/img/logo.png'}
-                        alt="Handbook AI"
-                        fill
-                    />
+            {!IS_MESSAGE_PAGE && !openChat && (
+                <Button onClick={() => setOpenChat((prev) => !prev)} className="h-10 w-10">
+                    <Image src={'/assets/img/logo.png'} alt="Handbook AI" fill />
                 </Button>
             )}
 
@@ -131,16 +121,11 @@ const ChatWithAI = () => {
                                     />
 
                                     <div className="flex flex-col">
-                                        <h3 className="ml-2 text-sm">
-                                            Trò chuyện cùng Handbook AI (Beta)
-                                        </h3>
+                                        <h3 className="ml-2 text-sm">Trò chuyện cùng Handbook AI (Beta)</h3>
                                     </div>
                                 </div>
 
-                                <Button
-                                    variant={'secondary'}
-                                    onClick={() => setOpenChat(false)}
-                                >
+                                <Button variant={'secondary'} onClick={() => setOpenChat(false)}>
                                     <svg
                                         xmlns="http://www.w3.org/2000/svg"
                                         className="h-6 w-6"
@@ -169,35 +154,27 @@ const ChatWithAI = () => {
                                 {messages.map((msg, index) => (
                                     <div
                                         key={index}
-                                        className={cn(
-                                            'mb-2 flex flex-col items-start',
-                                            {
-                                                'items-end': !msg.isAI,
-                                            }
-                                        )}
+                                        className={cn('mb-2 flex flex-col items-start', {
+                                            'items-end': !msg.isAI,
+                                        })}
                                     >
                                         <div
                                             className={cn(
                                                 'flex max-w-[70%] items-center whitespace-pre-wrap rounded-lg px-2 py-1 text-sm',
                                                 {
-                                                    'bg-primary-1 dark:bg-dark-secondary-2':
-                                                        msg.isAI,
-                                                    'bg-primary-2 text-white':
-                                                        !msg.isAI,
+                                                    'bg-primary-1 dark:bg-dark-secondary-2': msg.isAI,
+                                                    'bg-primary-2 text-white': !msg.isAI,
                                                 }
                                             )}
                                             dangerouslySetInnerHTML={{
-                                                __html: DOMPurify.sanitize(
-                                                    msg.text
-                                                ),
+                                                __html: DOMPurify.sanitize(msg.text),
                                             }}
                                         />
                                     </div>
                                 ))}
 
                                 <p className="mx-4 text-center text-xs text-gray-500 dark:text-gray-400">
-                                    Dữ liệu đoạn chat này không được lưu trữ và
-                                    sẽ biến mất khi bạn rời khỏi trang.
+                                    Dữ liệu đoạn chat này không được lưu trữ và sẽ biến mất khi bạn rời khỏi trang.
                                 </p>
                             </div>
 
@@ -210,8 +187,7 @@ const ChatWithAI = () => {
                                     <input
                                         {...register('text', {
                                             required: {
-                                                message:
-                                                    'Vui lòng nhập bình luận',
+                                                message: 'Vui lòng nhập bình luận',
                                                 value: true,
                                             },
                                         })}
@@ -222,16 +198,8 @@ const ChatWithAI = () => {
                                         autoComplete="off"
                                     />
 
-                                    <Button
-                                        className="rounded-none border-l text-base"
-                                        variant={'event'}
-                                        type="submit"
-                                    >
-                                        {isLoading ? (
-                                            <Icons.Loading className="animate-spin" />
-                                        ) : (
-                                            <Icons.Send />
-                                        )}
+                                    <Button className="rounded-none border-l text-base" variant={'event'} type="submit">
+                                        {isLoading ? <Icons.Loading className="animate-spin" /> : <Icons.Send />}
                                     </Button>
                                 </div>
                             </form>
