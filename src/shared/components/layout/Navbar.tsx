39d9ec5d64ca44ab6383e545234f76162c7acce1
Ngô Huy <117896799+huydotcode@users.noreply.github.com>
2026-04-02 15:05:27 +0700

    chore: change z-index ui navbar

--- src/shared/components/layout/Navbar.tsx
+++ src/shared/components/layout/Navbar.tsx
@@ -3,12 +3,7 @@ import { useAuth } from '@/core/context/AuthContext';
 import { useSidebarCollapse } from '@/core/context/SidebarContext';
 import { cn } from '@/lib/utils';
 import { Button } from '@/shared/components/ui/Button';
-import {
-    Tooltip,
-    TooltipContent,
-    TooltipProvider,
-    TooltipTrigger,
-} from '@/shared/components/ui/tooltip';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/shared/components/ui/tooltip';
 import { navbarLink } from '@/shared/constants';
 import { USER_ROLES } from '@/types/entites';
 import Link from 'next/link';
@@ -24,7 +19,7 @@ const Navbar = () => {
     const { setIsSidebarOpen } = useSidebarCollapse();
 
     return (
-        <nav className="fixed left-0 right-0 top-0 z-50 h-[--navbar-height] w-screen shadow-md md:px-2">
+        <nav className="fixed left-0 right-0 top-0 z-10 h-[--navbar-height] w-screen shadow-md md:px-2">
             <div className="relative flex h-full w-full items-center justify-between px-5 md:px-1">
                 <div className="flex w-1/4 items-center md:w-1/2">
                     <div className={'flex items-center justify-center'}>
@@ -54,17 +49,11 @@ const Navbar = () => {
                         }
                     >
                         {navbarLink.map((link) => {
-                            if (
-                                link.role === USER_ROLES.ADMIN &&
-                                user?.role !== USER_ROLES.ADMIN
-                            )
-                                return null;
+                            if (link.role === USER_ROLES.ADMIN && user?.role !== USER_ROLES.ADMIN) return null;
 
                             const isActived =
                                 path === link.path ||
-                                (path.includes(link.path) &&
-                                    link.path !== '/' &&
-                                    !path.includes('/admin'));
+                                (path.includes(link.path) && link.path !== '/' && !path.includes('/admin'));
                             const Icon = () => {
                                 return link.icon;
                             };
@@ -77,8 +66,7 @@ const Navbar = () => {
                                                 className={cn(
                                                     `flex h-full w-full cursor-pointer items-center p-2 hover:bg-hover-2 dark:hover:bg-dark-hover-1 md:rounded-xl`,
                                                     {
-                                                        'border-b-4 border-b-blue':
-                                                            isActived,
+                                                        'border-b-4 border-b-blue': isActived,
                                                     }
                                                 )}
                                             >
@@ -86,24 +74,19 @@ const Navbar = () => {
                                                     className={cn(
                                                         'flex h-full w-full items-center justify-center dark:text-dark-primary-1 md:justify-start',
                                                         {
-                                                            'text-blue dark:text-blue':
-                                                                isActived,
+                                                            'text-blue dark:text-blue': isActived,
                                                         }
                                                     )}
                                                     href={link.path || '/'}
                                                 >
                                                     <Icon />
 
-                                                    <span className="ml-2 hidden text-xs md:block">
-                                                        {link.name}
-                                                    </span>
+                                                    <span className="ml-2 hidden text-xs md:block">{link.name}</span>
                                                 </Link>
                                             </li>
                                         </TooltipTrigger>
 
-                                        <TooltipContent>
-                                            {link.name}
-                                        </TooltipContent>
+                                        <TooltipContent>{link.name}</TooltipContent>
                                     </Tooltip>
                                 </TooltipProvider>
                             );
